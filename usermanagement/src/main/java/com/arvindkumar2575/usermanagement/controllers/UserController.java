1ca615ba3af308bcc14c61715545c8a79a9179c7
Arvind Kumar <arvindkumar2575@gmail.com>
2023-03-12 14:18:58 +0530

    add get request

--- usermanagement/src/main/java/com/arvindkumar2575/usermanagement/controllers/UserController.java
+++ usermanagement/src/main/java/com/arvindkumar2575/usermanagement/controllers/UserController.java
@@ -3,9 +3,12 @@ package com.arvindkumar2575.usermanagement.controllers;
 import com.arvindkumar2575.usermanagement.entities.User;
 import com.arvindkumar2575.usermanagement.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,5 +19,10 @@ public class UserController {
     @PostMapping("/users")
     public User saveUser(@RequestBody User user){
         return userService.saveUser(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getUser(){
+        return userService.getUser();
     }
 }
--- usermanagement/src/main/java/com/arvindkumar2575/usermanagement/services/UserService.java
+++ usermanagement/src/main/java/com/arvindkumar2575/usermanagement/services/UserService.java
@@ -2,6 +2,10 @@ package com.arvindkumar2575.usermanagement.services;
 
 import com.arvindkumar2575.usermanagement.entities.User;
 
+import java.util.List;
+
 public interface UserService {
     User saveUser(User user);
+
+    List<User> getUser();
 }
